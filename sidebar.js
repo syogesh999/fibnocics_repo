e059b2fca7facd7cd76f06140062e100543e7c3c
syogesh999 <147244758+syogesh999@users.noreply.github.com>
2024-04-11 17:49:46 +0530

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+document.getElementById("mySidenav").style.width = "0";
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px"; // Adjust the width as needed
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
